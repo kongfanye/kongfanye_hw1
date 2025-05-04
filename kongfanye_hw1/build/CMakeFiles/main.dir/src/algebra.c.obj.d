3e6e3c01b6079a9076c4a83c4a67a5bac14ddc2f
kongfanye <1595621152@qq.com>
2025-05-05 00:11:41 +0800

    Add files via upload

---
+++ kongfanye_hw1/build/CMakeFiles/main.dir/src/algebra.c.obj.d
@@ -0,0 +1,15 @@
+CMakeFiles/main.dir/src/algebra.c.obj: D:\kongfanye_hw1\src\algebra.c \
+ D:/kongfanye_hw1/inc/algebra.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/stdio.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/corecrt.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/_mingw.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/vadefs.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/PROGRA~2/EMBARC~1/Dev-Cpp/TDM-GC~1/x86_64-w64-mingw32/include/math.h
